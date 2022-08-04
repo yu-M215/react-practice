25d81a1a0c791a0608eab67c80a7f8253d5ad48a
yu-M215 <yuu0609km.215@gmail.com>
2022-08-04 19:25:18 +0900

    Rectコンポーネントを利用する

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Rect from './React';
 
 class App extends Component {
 
-  constructor(props){
-    super()
-    this.title = props.title
-    this.message = props.message
-  }
-
-  render() {
+  render(){
     return <div>
-      <h1 className='bg-primary text-white display-4'>React</h1>
+      <h1 className='bg-primaty text-white display-4'>React</h1>
       <div className='container'>
-        <p className='subtitle'>{this.title}</p>
-        <p>{this.message}</p>
+        <p className='subtitle'>draw rectangle.</p>
+        <Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
+        <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
+        <Rect x="400" y="400" w="200" h="200" c="#6669" r="100" />
       </div>
     </div>
   }
