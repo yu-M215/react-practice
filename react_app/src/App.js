2aad1275cb9078cefd242ad04bc78d53b2692966
yu-M215 <yuu0609km.215@gmail.com>
2022-08-04 23:02:52 +0900

    ステートを用意する

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Rect from './React';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      msg:'Hello Component.',
+    }
+  }
 
   render(){
     return <div>
-      <h1 className='bg-primaty text-white display-4'>React</h1>
+      <h1 className='bg-primary text-white display-4'>React</h1>
       <div className='container'>
-        <p className='subtitle'>draw rectangle.</p>
-        <Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
-        <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
-        <Rect x="400" y="400" w="200" h="200" c="#6669" r="100" />
+        <p className='subtitle'>Show message.</p>
+        <p className='alert alert-warning'>{this.state.msg}</p>
+        <p className='alert alert-dark'>{this.props.msg}</p>
       </div>
     </div>
   }
