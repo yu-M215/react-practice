c879f23e8c29035a89d57da02302102ecc15b361
yu-M215 <yuu0609km.215@gmail.com>
2022-08-04 23:06:54 +0900

    ステートを更新する

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import Rect from './React';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      msg:'Hello Component.',
+      msg:'Hello.',
+      count:0,
     }
+    let timer = setInterval(()=>{
+      this.setState({
+        count: this.state.count + 1,
+        msg: "[ count: " + this.state.count + " ]"
+      })
+    }, 1000)
   }
 
   render(){
     return <div>
       <h1 className='bg-primary text-white display-4'>React</h1>
       <div className='container'>
-        <p className='subtitle'>Show message.</p>
+        <p className='subtitle'>Count number.</p>
         <p className='alert alert-warning'>{this.state.msg}</p>
         <p className='alert alert-dark'>{this.props.msg}</p>
       </div>
